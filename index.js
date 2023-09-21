1527c3dd3e85e0494e76789848e893f1bf6efab3
Fellipe <ramosfellipe630@gmail.com>
2023-09-21 12:04:12 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 import express from 'express';
-import {retornaColecaoHistoricoInflacao, buscaAnoHistoricoInflacao, buscaIdHistoricoInflacao,calcularReajuste} from './funcoes/servico.js';
+import {retornaColecaoHistoricoInflacao, buscaAnoHistoricoInflacao, buscaIdHistoricoInflacao,calcularReajuste,validacaoErro} from './funcoes/servico.js';
 
 const app = express();
-
-// primeira rota retorna a lista completa
-// A segunda rota da API deve retornar um histórico de dados da coleção, referente a um ano específico
-
-app.get('/historicoIPCA', (req, res) => {
-    const buscaAno = req.query.ano;
-    const filtraAno = buscaAno ? buscaAnoHistoricoInflacao(buscaAno) : retornaColecaoHistoricoInflacao();
-
-    if (filtraAno.length > 0) {
-        res.json(filtraAno);
-      } else {
-        res.status(404).send({ "erro": "Nenhuma Ano foi encontrado" });
-      };
-});
-
-//A terceira rota da API deve retornar um elemento da coleção, conforme o código de identificação (id) do elemento
-
-app.get('/historicoIPCA/:id', (req, res) => {
-    const Id = buscaIdHistoricoInflacao(req.params.id);
-
-    if(Id){
-        res.json(Id);
-    }else if(isNaN(parseInt(req.params.id))){
-        res.status(400).send({"Erro": "Id invalido"})
-    }else{
-        res.status(404).send({"Erro": "Id nao encontrado"})
-    }
-});
-
-//A quarta rota da API deve retornar um cálculo de reajuste
 
 app.get('/historicoIPCA/calculo', (req, res) => {
 
@@ -41,12 +11,46 @@ app.get('/historicoIPCA/calculo', (req, res) => {
     const dataFinalMes = parseInt(req.query.mesFinal);
     const dataFinalAno = parseInt(req.query.anoFinal); 
 
-    const resultado = calcularReajuste(valor,dataInicialMes,dataInicialAno,dataFinalMes,dataFinalAno);
-
-    res.json({resultado:resultado});
+    if (validacaoErro(valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno)) {
+        res.status(400).json({ erro: 'Parâmetros inválidos' });
+        return;
+      }
+    
+      const resultado = calcularReajuste(valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno);
+      res.json({ resultado: resultado });
 });
 
 
+app.get('/historicoIPCA/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        res.status(404).json({ erro: 'ID inválido' });
+        return;
+    };
+
+    const idResultado = buscaIdHistoricoInflacao(id);
+    if(idResultado){
+        res.json(idResultado);
+    }else{
+        res.status(404).json({erro:'Id nao encontrado'})
+    };
+});
+
+
+app.get('/historicoIPCA', (req, res) => {
+    const ano = parseInt(req.query.ano);
+    if(isNaN(ano)){
+        res.status(404).json({erro: 'valor passado invalido'});
+    }else{
+        const filtraAno = buscaAnoHistoricoInflacao(ano);
+        if (filtraAno.length > 0) {
+            res.json(filtraAno);
+          } else {
+            res.status(404).json({ erro: "Nenhuma Ano foi encontrado" });
+          };
+    }
+});
 
 app.listen(8080, () => {
     console.log('Api de IPA inicializando..');
